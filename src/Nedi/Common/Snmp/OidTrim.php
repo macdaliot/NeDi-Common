8529e3f542ea33947e882d409775f32bbe116974
Frank Hildebrandt <frank.hildebrandt@me.com>
2014-04-15 09:40:48 +0200

    Added a oid trimmer

---
+++ src/Nedi/Common/Snmp/OidTrim.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Nedi\Common\Snmp;
+
+
+class OidTrim
+{
+
+    /**
+     * @param $array
+     * @return mixed
+     */
+    protected function trim($array)
+    {
+        $result = array();
+        foreach ($array as $key => $row) {
+            $result[$this->getKey($key)] = $row;
+        }
+        return $result;
+    }
+
+    /**
+     * @param $oid
+     * @return mixed
+     */
+    protected function getKey($oid)
+    {
+        $array = explode(".", $oid);
+        return array_pop($array);
+    }
+}
